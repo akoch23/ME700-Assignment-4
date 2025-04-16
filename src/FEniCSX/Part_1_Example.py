253a7bc9779c38ef0cc4703313895d2d4c1a64c3
akoch23 <akoch23@bu.edu>
2025-04-16 04:06:06 -0400

    Update Part_1_Example.py

--- src/FEniCSX/Part_1_Example.py
+++ src/FEniCSX/Part_1_Example.py
@@ -1,1 +1,128 @@
+import gmsh
+import dolfinx.io
+import numpy as np
+import matplotlib.pyplot as plt
+import ufl
+import pyvista
 
+from dolfinx.io import gmshio
+from dolfinx.fem.petsc import LinearProblem
+from mpi4py import MPI
+from dolfinx import fem
+from dolfinx import default_scalar_type
+from dolfinx.plot import vtk_mesh
+from dolfinx import geometry
+from pathlib import Path
+
+gmsh.initialize()
+membrane = gmsh.model.occ.addDisk(0, 0, 0, 1, 1)
+gmsh.model.occ.synchronize()
+gdim = 2
+gmsh.model.addPhysicalGroup(gdim, [membrane], 1)
+gmsh.option.setNumber("Mesh.CharacteristicLengthMin", 0.05)
+gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 0.05)
+gmsh.model.mesh.generate(gdim)
+
+gmsh_model_rank = 0
+mesh_comm = MPI.COMM_WORLD
+domain, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, gmsh_model_rank, gdim=gdim)
+
+V = fem.functionspace(domain, ("Lagrange", 1))
+
+x = ufl.SpatialCoordinate(domain)
+beta = fem.Constant(domain, default_scalar_type(12))
+R0 = fem.Constant(domain, default_scalar_type(0.3))
+p = 4 * ufl.exp(-beta**2 * (x[0]**2 + (x[1] - R0)**2))
+
+
+def on_boundary(x):
+    return np.isclose(np.sqrt(x[0]**2 + x[1]**2), 1)
+
+
+boundary_dofs = fem.locate_dofs_geometrical(V, on_boundary)
+
+bc = fem.dirichletbc(default_scalar_type(0), boundary_dofs, V)
+
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+a = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
+L = p * v * ufl.dx
+problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+
+Q = fem.functionspace(domain, ("Lagrange", 5))
+expr = fem.Expression(p, Q.element.interpolation_points())
+pressure = fem.Function(Q)
+pressure.interpolate(expr)
+
+
+pyvista.start_xvfb()
+
+# Extract topology from mesh and create pyvista mesh
+topology, cell_types, x = vtk_mesh(V)
+grid = pyvista.UnstructuredGrid(topology, cell_types, x)
+
+# Set deflection values and add it to plotter
+grid.point_data["u"] = uh.x.array
+warped = grid.warp_by_scalar("u", factor=25)
+
+plotter = pyvista.Plotter()
+plotter.add_mesh(warped, show_edges=True, show_scalar_bar=True, scalars="u")
+if not pyvista.OFF_SCREEN:
+    plotter.show()
+else:
+    plotter.screenshot("deflection.png")
+
+load_plotter = pyvista.Plotter()
+p_grid = pyvista.UnstructuredGrid(*vtk_mesh(Q))
+p_grid.point_data["p"] = pressure.x.array.real
+warped_p = p_grid.warp_by_scalar("p", factor=0.5)
+warped_p.set_active_scalars("p")
+load_plotter.add_mesh(warped_p, show_scalar_bar=True)
+load_plotter.view_xy()
+if not pyvista.OFF_SCREEN:
+    load_plotter.show()
+else:
+    load_plotter.screenshot("load.png")
+
+tol = 0.001  # Avoid hitting the outside of the domain
+y = np.linspace(-1 + tol, 1 - tol, 101)
+points = np.zeros((3, 101))
+points[1] = y
+u_values = []
+p_values = []
+
+bb_tree = geometry.bb_tree(domain, domain.topology.dim)
+
+cells = []
+points_on_proc = []
+# Find cells whose bounding-box collide with the the points
+cell_candidates = geometry.compute_collisions_points(bb_tree, points.T)
+# Choose one of the cells that contains the point
+colliding_cells = geometry.compute_colliding_cells(domain, cell_candidates, points.T)
+for i, point in enumerate(points.T):
+    if len(colliding_cells.links(i)) > 0:
+        points_on_proc.append(point)
+        cells.append(colliding_cells.links(i)[0])
+
+points_on_proc = np.array(points_on_proc, dtype=np.float64)
+u_values = uh.eval(points_on_proc, cells)
+p_values = pressure.eval(points_on_proc, cells)
+
+fig = plt.figure()
+plt.plot(points_on_proc[:, 1], 50 * u_values, "k", linewidth=2, label="Deflection ($\\times 50$)")
+plt.plot(points_on_proc[:, 1], p_values, "b--", linewidth=2, label="Load")
+plt.grid(True)
+plt.xlabel("y")
+plt.legend()
+# If run in parallel as a python file, we save a plot per processor
+plt.savefig(f"membrane_rank{MPI.COMM_WORLD.rank:d}.png")
+
+pressure.name = "Load"
+uh.name = "Deflection"
+results_folder = Path("results")
+results_folder.mkdir(exist_ok=True, parents=True)
+with dolfinx.io.VTXWriter(MPI.COMM_WORLD, results_folder / "membrane_pressure.bp", [pressure], engine="BP4") as vtx:
+    vtx.write(0.0)
+with dolfinx.io.VTXWriter(MPI.COMM_WORLD, results_folder / "membrane_deflection.bp", [uh], engine="BP4") as vtx:
+    vtx.write(0.0)
