76f6239bc7776f6dba11bb5b1b8f3c14dab3f428
akoch23 <akoch23@bu.edu>
2025-05-07 04:20:45 -0400

    Update Part_2_a.py

--- src/FEniCSX/Part2/Part_2_a.py
+++ src/FEniCSX/Part2/Part_2_a.py
@@ -3,18 +3,23 @@
 # Imports for specific functions
 from mpi4py import MPI # Message Passing Interface, required for parallel processing
 from dolfinx import mesh # Mesh generation class within DolfinX
-
+from dolfinx import fem
 # Define discrete domain (construct singular mesh, parallel processing)
 domain = mesh.create_unit_square(MPI.COMM_WORLD, 8, 8, mesh.CellType.quadrilateral) # Creates a square mesh from 8 x 8 quadrilateral elements, mesh generation is distributed across all available processors
-mpirun -n 2 Part_2_a.py # Alters above command, script will specifically use 2 processors
+# mpirun -n 2 Part_2_a.py # Alters above command, script will specifically use 2 processors
 
 # Create finite element function space
 from dolfinx.fem import functionspace
 V = functionspace(domain, ("Lagrange", 1)) # Defines a function space over the domain, using Langrange elements of degree 1 (piecewise linear elements)
-from dolfinx import fem
+from dolfinx.fem import Function, Expression
+from ufl import SpatialCoordinate
 # Function definition
-uD = fem.Function(V) # Specifies Finite Element Function over space V
-uD = interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2) # Interpolate is used to set the the FE function to the known analytical solution
+uD = Function(V) # Specifies Finite Element Function over space V
+# expr = Expression(lambda x: 1 + x[0]**2 + 2 * x[1]**2, V.element.interpolation_points()) # Interpolate is used to set the the FE function to the known analytical solution
+x = SpatialCoordinate(domain)
+ufl_expr = 1 + x[0]**2 + 2 * x[1]**2
+expr = Expression(ufl_expr, V.element.interpolation_points())
+uD.interpolate(expr)
 
 import numpy
 # Create facet to cell connectivity required to determine boundary facets
@@ -46,9 +51,14 @@ problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "p
 uh = problem.solve() # Numerical solution of the Poisson equation after solving the linear problem
 
 # Computing Solution Error (Validation Step)
-V2 = fem.functionspace(domain, ("Lagrange", 2)) # Defines higher-degree function space (L^2 elements, degree 2 instead of degree 1) for the exact solution
-uex = fem.Function(V2) # Defines Finite Element Function over space V2
-uex.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2) # Interpolates the analytical solution into the higher-order function space
+from dolfinx.fem import Expression
+V2 = functionspace(domain, ("Lagrange", 2)) # Defines higher-degree function space (L^2 elements, degree 2 instead of degree 1) for the exact solution
+uex = Function(V2) # Defines Finite Element Function over space V2
+# expr = Expression(lambda x: 1 + x[0]**2 + 2 * x[1]**2, V2.element.interpolation_points()) # Interpolates the analytical solution into the higher-order function space
+x = SpatialCoordinate(domain)
+ufl_expr2 = 1 + x[0]**2 + 2 * x[1]**2
+expr2 = Expression(ufl_expr2, V2.element.interpolation_points())
+uex.interpolate(expr2)
 
 L2_error = fem.form(ufl.inner(uh - uex, uh - uex) * ufl.dx) # Defines error in L^2 norm by the difference of the numerical solution (uh) and the exact solution (uex)
 error_local = fem.assemble_scalar(L2_error)
@@ -62,38 +72,49 @@ if domain.comm.rank == 0:
 
 # Plot mesh using pyvista (Python library for 3D visualization)
 import pyvista 
-print(pyvista.global_theme.jupyter_backend)
-
 from dolfinx import plot
+
 pyvista.start_xvfb()
 domain.topology.create_connectivity(tdim, tdim)
 topology, cell_types, geometry = plot.vtk_mesh(domain, tdim) # Converts mesh into plottable format for PyVista
 grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
 
 # Visualise Mesh
-plotter = pyvista.Plotter()
+plotter = pyvista.Plotter(off_screen=True)
 plotter.add_mesh(grid, show_edges=True) # Adds mesh to plot and visualizes it
 plotter.view_xy()
-if not pyvista.OFF_SCREEN:
-    plotter.show()
-else:
-    figure = plotter.screenshot("fundamentals_mesh.png")
+plotter.render()
+plotter.screenshot("grid_normal.png")
+plotter.close()
+print("2D image of undeformed unloaded grid generated.")
 
 
 # Plot function using pyvista
-u_topology, u_cell_types, u_geometry = plot.vtk_mesh(V) 
-u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry)
-u_grid.point_data["u"] = uh.x.array.real # Assigns computed solution values to the mesh
-u_grid.set_active_scalars("u")
-u_plotter = pyvista.Plotter()
-u_plotter.add_mesh(u_grid, show_edges=True) # Plots the solution using PyVista
-u_plotter.view_xy()
-if not pyvista.OFF_SCREEN:
-    u_plotter.show()
+topology, cell_types, geometry = plot.vtk_mesh(V) 
+grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
+grid.point_data["u"] = uh.x.array.real # Assigns computed solution values to the mesh
+grid.set_active_scalars("u")
+plotter = pyvista.Plotter(off_screen=True)
+plotter.add_mesh(grid, show_edges=True) # Plots the solution using PyVista
+plotter.view_xy()
+plotter.view_xy()
+plotter.render()
+plotter.screenshot("grid_deformed.png")
+plotter.close()
+print("2D image of deformed grid generated.")
+
 
 # Warp mesh by scalar factor for 3D visualization
-warped = u_grid.warp_by_scalar() # Creates a 3D effect by warping the mesh by the solution values
-plotter2 = pyvista.Plotter()
-plotter2.add_mesh(warped, show_edges=True, show_scalar_bar=True)
-if not pyvista.OFF_SCREEN:
-    plotter2.show()
+warped = grid.warp_by_scalar() # Creates a 3D effect by warping the mesh by the solution values
+plotter = pyvista.Plotter(off_screen=True)
+plotter.add_mesh(warped, show_edges=True, show_scalar_bar=True)
+plotter.view_xy()
+plotter.camera_position = [
+    (5, 5, 5),   # camera location
+    (0, 0, 1),      # focal point (where the camera looks)
+    (1, 0, 1),      # up direction
+]
+plotter.render()
+plotter.screenshot("3Dgrid_deformed.png")
+plotter.close()
+print("3D image of deformed grid generated.")
